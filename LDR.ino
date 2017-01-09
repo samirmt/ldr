a8dcd3181803f6735d4becf24174ab6060696ff5
Samir M Teixeira <samate82@hotmail.com>
2017-01-09 13:59:33 -0200

    Add files via upload

---
+++ LDR.ino
@@ -0,0 +1,18 @@
+int LDR = A3;
+
+void setup(){
+	Serial.begin(9600); //Inicializa Comunicação Serial
+}
+
+void loop(){
+  int luz = analogRead(LDR); //Armazena na variavel luz a leitura
+  if(luz>500) {            // testa os valores   
+    Serial.println("Apagada");
+   }
+  else {
+    Serial.println("Acesa");
+  }
+	delay(1000);       //aguarda 1 segundo
+}
+
+
